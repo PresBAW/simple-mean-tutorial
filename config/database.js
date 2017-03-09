8e519e5b73d8beabe180795f3072bf0d5ceff21d
Benjamin Wallsten <bwallste@ucsc.edu>
2017-03-09 00:16:23 -0800

    First attempt at inserting mongodb uri into config/database.js file

--- config/database.js
+++ config/database.js
@@ -1,3 +1,13 @@
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
+
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     localUrl: 'mongodb://localhost/meanstacktutorials'
